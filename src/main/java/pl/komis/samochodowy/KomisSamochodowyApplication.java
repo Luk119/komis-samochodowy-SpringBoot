a8b58b81e4632468cd58ac90d2a962e52537d677
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-05-14 12:38:44 +0200

    App

---
+++ src/main/java/pl/komis/samochodowy/KomisSamochodowyApplication.java
@@ -0,0 +1,13 @@
+package pl.komis.samochodowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomisSamochodowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KomisSamochodowyApplication.class, args);
+    }
+
+}
